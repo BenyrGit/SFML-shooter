9d51a605268d6fe44ec51f041af6e8d83bbb4f07
Benjamin <benjamin.yr.paquette@gmail.com>
2026-06-16 06:32:21 -0400

    feat(Command): ajout d'un template pour exécuter les commandes

--- src/Command.hpp
+++ src/Command.hpp
@@ -11,7 +11,7 @@
  Historique de modifications :
  JJ-MM-AAAA     Nom                     Commentaire
  =========================================================
-
+ 16-06-2026     Benjamin Paquette       Ajout d'un template pour exécuter les commandes
  ****************************************/
 #pragma once
 
@@ -29,3 +29,14 @@ struct Command
     std::function<void(SceneNode&, sf::Time)> action;
     unsigned int category = Category::None;
 };
+
+template <typename GameObject, typename Function>
+std::function<void(SceneNode&, sf::Time)> derivedAction(Function function)
+{
+    return [=](SceneNode& node, sf::Time deltaTime)
+        {
+            assert(dynamic_cast<GameObject*>(&node) != nullptr);
+
+            function(static_cast<GameObject&>(node), deltaTime);
+        };
+}
