fd1009185e0f6abed9946ceb224ffa0ff230e1b6
Benjamin <benjamin.yr.paquette@gmail.com>
2026-06-22 10:01:42 -0400

    feat(LoadingState): ajout d'une barre de chargement

--- src/LoadingState.cpp
+++ src/LoadingState.cpp
@@ -6,6 +6,8 @@
 LoadingState::LoadingState(StateStack& stack, Context context)
     : State(stack, context)
     , mLoadingText(context.fonts->get(Fonts::ID::Main), "Chargement...", 45)
+    , mProgressBarBackground()
+    , mProgressBar()
 {
     const sf::Vector2f viewSize = context.window->getDefaultView().getSize();
 
@@ -13,6 +15,28 @@ LoadingState::LoadingState(StateStack& stack, Context context)
     mLoadingText.setPosition({
         viewSize.x / 2.f,
         viewSize.y / 2.f
+    });
+
+    mProgressBarBackground.setSize({ 400.f, 20.f });
+    mProgressBarBackground.setFillColor(sf::Color(80, 80, 80));
+    mProgressBarBackground.setOrigin({
+        mProgressBarBackground.getSize().x / 2.f,
+        mProgressBarBackground.getSize().y / 2.f
+        });
+    mProgressBarBackground.setPosition({
+        viewSize.x / 2.f,
+        viewSize.y / 2.f + 30.f
+        });
+
+    mProgressBar.setSize({ 0.f, 20.f });
+    mProgressBar.setFillColor(sf::Color::White);
+    mProgressBar.setOrigin({
+        0.f,
+        mProgressBar.getSize().y / 2.f
+        });
+    mProgressBar.setPosition({
+        viewSize.x / 2.f - 200.f,
+        viewSize.y / 2.f + 30.f
         });
 }
 
@@ -22,11 +46,19 @@ void LoadingState::draw()
 
     window.setView(window.getDefaultView());
     window.draw(mLoadingText);
+    window.draw(mProgressBarBackground);
+    window.draw(mProgressBar);
 }
 
-bool LoadingState::update(sf::Time)
+bool LoadingState::update(sf::Time deltaTime)
 {
-    if (!mGameStateCreated)
+    mElapsedTime += deltaTime;
+
+    // pourcentage 
+    const float percent = mElapsedTime.asSeconds() / mLoadingDuration.asSeconds();
+    setProgress(percent);
+
+    if (!mGameStateCreated && mElapsedTime >= mLoadingDuration)
     {
         mGameStateCreated = true;
 
@@ -40,4 +72,25 @@ bool LoadingState::update(sf::Time)
 bool LoadingState::handleEvent(const sf::Event&)
 {
     return true;
+}
+
+// Mise ;a jour de la barre
+void LoadingState::setProgress(float percent)
+{
+
+    // On garde la barre entre 0 et 100%
+    if (percent < 0.f)
+    {
+        percent = 0.f;
+    }
+    else if (percent > 1.f)
+    {
+        percent = 1.f;
+    }
+
+    mProgressBar.setSize({
+        400.f * percent,
+        mProgressBar.getSize().y
+    });
+
 }
--- src/LoadingState.hpp
+++ src/LoadingState.hpp
@@ -11,12 +11,13 @@
  Historique de modifications :
  JJ-MM-AAAA     Nom                     Commentaire
  =========================================================
-
+ 22-06-2026     Benjamin Paquette       Ajout d'une barre de chargement
  ****************************************/
 #pragma once
 
 #include "State.hpp"
 
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/Text.hpp>
 
 class LoadingState : public State
@@ -29,6 +30,17 @@ public:
     bool handleEvent(const sf::Event& event) override;
 
 private:
+    // Méthodes
+    void setProgress(float percent);
+
+    // attributs
     sf::Text mLoadingText;
-    bool mGameStateCreated = false;     // pour être évité de demandé plusieurs foit l'état Game
+
+    sf::RectangleShape mProgressBarBackground;
+    sf::RectangleShape mProgressBar;
+
+    sf::Time mElapsedTime = sf::Time::Zero;
+    sf::Time mLoadingDuration = sf::seconds(1.f);       // Temps de chargement simulée
+
+    bool mGameStateCreated = false;                     // pour être évité de demandé plusieurs foit l'état Game
 };
